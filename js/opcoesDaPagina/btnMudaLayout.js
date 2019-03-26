2cbbf84c78fbbd718965a55443565b3313598543
Caique Oliveira <caique_oliveira@outlook.com>
2019-03-25 22:32:41 -0300

    add-commit-aula01

---
+++ js/opcoesDaPagina/btnMudaLayout.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector('#btnMudaLayout') 
+function mudaTexto(){
+    if(btnMudaLayout.textContent.trim() ==="Linhas"){
+        btnMudaLayout.textContent ="Blocos"
+    }else{
+        btnMudaLayout.textContent="Linhas"
+
+    }
+}
+
+btnMudaLayout.addEventListener('click',mudaTexto)
+const mural = document.querySelector('.mural')
+    function mudaLayout(){
+
+    
+    mural.classList.toggle('mural--linha')
+}
+btn.addEventListener('click', mudaLayout)
+
